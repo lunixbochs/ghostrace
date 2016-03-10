aecc6ff7babff53f793121e7032b9a2fac5f1ea4
Ryan Hileman <lunixbochs@gmail.com>
2016-03-10 09:21:57 -0800

    make cli more generic

--- cli.go
+++ cli.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
@@ -9,38 +8,12 @@ import (
 )
 
 func main() {
-	fs := flag.NewFlagSet("ghostrace", flag.ExitOnError)
-	follow := fs.Bool("f", false, "follow subprocesses")
-	pid := fs.Int("p", -1, "attach to pid")
-	fs.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [options] -p <pid> | <exe> [args...]\n", os.Args[0])
-		fs.PrintDefaults()
-	}
-	fs.Parse(os.Args[1:])
-	args := fs.Args()
-
-	var trace chan *ghost.Event
-	var err error
-	tracer := ghost.NewTracer()
-	if pid != nil && *pid >= 0 {
-		trace, err = tracer.Trace(*pid)
-	} else {
-		if len(args) > 0 {
-			trace, err = tracer.Spawn(args[0], args...)
-		} else {
-			fs.Usage()
-			os.Exit(1)
-		}
-	}
+	trace, err := ghost.DefaultCli(os.Args, nil)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error starting trace: %s\n", err)
+		fmt.Printf("%s\n", err)
 		os.Exit(1)
 	}
-	tracer.ExecFilter(func(c *ghost.Event) (bool, bool) {
-		// fmt.Println("exec filter", c)
-		// keepParent, followChild
-		return true, *follow
-	})
+	// TODO: no way to kill target, so this should be more than a channel
 	for sc := range trace {
 		fmt.Fprintf(os.Stderr, "%+v\n", sc)
 	}
